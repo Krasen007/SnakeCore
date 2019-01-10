1a2d74723322100264032888547cfe18e36a45dd
Krasen Ivanov <krasen007@gmail.com>
2019-01-10 22:32:53 +0200

    Some optimization for snake delete on edge case

--- View/SnakeView.cs
+++ View/SnakeView.cs
@@ -45,40 +45,73 @@
                 Console.Write(headSymbol);
             }
 
-            for (int i = 1; i < this.snake.SnakeElements.Count; i++)
+            if (this.snake.SnakeElements.Count > 1)
             {
-                Console.SetCursorPosition(this.snake.SnakeElements[i].X, this.snake.SnakeElements[i].Y);
-                Console.Write("*");
+                if ((this.snake.SnakeElements[1].X == 49) && (this.snake.SnakeElements[1].Y == 24))
+                {
+                    // test fix
+                    Console.SetCursorPosition(5, 5);
+                }
+                else
+                {
+                    for (int i = 1; i < this.snake.SnakeElements.Count; i++)
+                    {
+                        if ((this.snake.SnakeElements[i].X == 49) && (this.snake.SnakeElements[i].Y == 24))
+                        {
+                            Console.SetCursorPosition(5, 5);
+                        } else
+                        {
+                            for (int j = 1; j < this.snake.SnakeElements.Count; j++)
+                            {
+                                Console.SetCursorPosition(this.snake.SnakeElements[i].X, this.snake.SnakeElements[i].Y);
+                                Console.Write("*");
+                            }
+                        }
+                    }                    
+                }
             }
+            //else
+            //{
+            //    if ((this.snake.SnakeElements[0].X == 49) && (this.snake.SnakeElements[0].Y == 24))
+            //    {
+            //        // test fix
+            //        Console.SetCursorPosition(5, 5);
+            //    } else
+            //    {
+            //        Console.SetCursorPosition(this.snake.SnakeElements[0].X, this.snake.SnakeElements[0].Y);
+            //        Console.Write(headSymbol);
+            //    }
+            //}
         }
 
         public void Delete()
         {
-            ////for (int i = 0/*this.snake.SnakeElements.Count - 4*/; i < this.snake.SnakeElements.Count; i++)
-            ////{
-            ////    Console.SetCursorPosition(this.snake.SnakeElements[i].X, this.snake.SnakeElements[i].Y);
-            ////    Console.Write(" ");
-            ////}
+            if (this.snake.SnakeElements.Count > 1)
+            {
+                for (int i = 1; i < this.snake.SnakeElements.Count; i++)
+                {
+                    if ((this.snake.SnakeElements[i].X == 49) && (this.snake.SnakeElements[i].Y == 24))
+                    {
+                        // test fix
+                        Console.SetCursorPosition(5, 5);
+                    }
+                    else
+                    {
+                        Console.SetCursorPosition(
+                        this.snake.SnakeElements[this.snake.SnakeElements.Count - 1].X,
+                        this.snake.SnakeElements[this.snake.SnakeElements.Count - 1].Y);
+                        Console.Write(" ");
+                    }
+                }
 
-            Console.SetCursorPosition(
+            }
+            else
+            {
+                Console.SetCursorPosition(
                     this.snake.SnakeElements[this.snake.SnakeElements.Count - 1].X,
                     this.snake.SnakeElements[this.snake.SnakeElements.Count - 1].Y);
-            Console.Write(" ");
-
-            ////if ((this.snake.SnakeElements[0].X == 49) && (this.snake.SnakeElements[0].Y == 24))
-            ////{
-            ////    Console.SetCursorPosition(
-            ////        this.snake.SnakeElements[this.snake.SnakeElements.Count - 1].X,
-            ////        this.snake.SnakeElements[this.snake.SnakeElements.Count - 1].Y);
-            ////    Console.Write("");
-            ////}
-            ////else
-            ////{
-            ////    Console.SetCursorPosition(
-            ////        this.snake.SnakeElements[this.snake.SnakeElements.Count - 1].X,
-            ////        this.snake.SnakeElements[this.snake.SnakeElements.Count - 1].Y);
-            ////    Console.Write(" ");
-            ////}
+                Console.Write(" ");
+            }
         }
     }
 }
