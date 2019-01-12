ab971e38e6fc36df10e0aaa7bb30bee801a81be8
Krasen Ivanov <krasen007@gmail.com>
2019-01-13 00:03:39 +0200

    Fix bug on snake deletion edge case

--- View/SnakeView.cs
+++ View/SnakeView.cs
@@ -45,54 +45,37 @@
                 Console.Write(headSymbol);
             }
 
-            if (this.snake.SnakeElements.Count > 1)
+            for (int i = 1; i < this.snake.SnakeElements.Count; i++)
             {
-                if ((this.snake.SnakeElements[1].X == 49) && (this.snake.SnakeElements[1].Y == 24))
+                if (this.snake.SnakeElements.Count > 1)
+                {
+                    if ((this.snake.SnakeElements[i].X == 49) && (this.snake.SnakeElements[i].Y == 24))
+                    {
+                        // test fix
+                        Console.SetCursorPosition(5, 5);
+                    }
+                    else
+                    {
+                        Console.SetCursorPosition(this.snake.SnakeElements[i].X, this.snake.SnakeElements[i].Y);
+                        Console.Write("*");
+                    }
+                }
+            }
+        }
+
+        public void Delete()
+        {
+            for (int i = 0; i < this.snake.SnakeElements.Count; i++)
+            {
+                if ((this.snake.SnakeElements[i].X == 49) && (this.snake.SnakeElements[i].Y == 24))
                 {
                     // test fix
                     Console.SetCursorPosition(5, 5);
                 }
                 else
                 {
-                    for (int i = 1; i < this.snake.SnakeElements.Count; i++)
+                    if ((this.snake.SnakeElements[this.snake.SnakeElements.Count - 1].X == 49) && (this.snake.SnakeElements[this.snake.SnakeElements.Count - 1].Y == 24))
                     {
-                        if ((this.snake.SnakeElements[i].X == 49) && (this.snake.SnakeElements[i].Y == 24))
-                        {
-                            Console.SetCursorPosition(5, 5);
-                        } else
-                        {
-                            for (int j = 1; j < this.snake.SnakeElements.Count; j++)
-                            {
-                                Console.SetCursorPosition(this.snake.SnakeElements[i].X, this.snake.SnakeElements[i].Y);
-                                Console.Write("*");
-                            }
-                        }
-                    }                    
-                }
-            }
-            //else
-            //{
-            //    if ((this.snake.SnakeElements[0].X == 49) && (this.snake.SnakeElements[0].Y == 24))
-            //    {
-            //        // test fix
-            //        Console.SetCursorPosition(5, 5);
-            //    } else
-            //    {
-            //        Console.SetCursorPosition(this.snake.SnakeElements[0].X, this.snake.SnakeElements[0].Y);
-            //        Console.Write(headSymbol);
-            //    }
-            //}
-        }
-
-        public void Delete()
-        {
-            if (this.snake.SnakeElements.Count > 1)
-            {
-                for (int i = 1; i < this.snake.SnakeElements.Count; i++)
-                {
-                    if ((this.snake.SnakeElements[i].X == 49) && (this.snake.SnakeElements[i].Y == 24))
-                    {
-                        // test fix
                         Console.SetCursorPosition(5, 5);
                     }
                     else
@@ -103,14 +86,6 @@
                         Console.Write(" ");
                     }
                 }
-
-            }
-            else
-            {
-                Console.SetCursorPosition(
-                    this.snake.SnakeElements[this.snake.SnakeElements.Count - 1].X,
-                    this.snake.SnakeElements[this.snake.SnakeElements.Count - 1].Y);
-                Console.Write(" ");
             }
         }
     }
